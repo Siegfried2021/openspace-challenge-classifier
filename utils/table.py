483a6db4ac42b0e685e820932071122d19bd35c9
carpanalper <alperc.business@gmail.com>
2024-06-13 14:49:19 +0200

    created Table class

--- utils/table.py
+++ utils/table.py
@@ -1,1 +1,47 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Table:
+    def __init__(self, capacity : int):
+        self.capacity = capacity 
+        self.seats:[Seat(i + 1) for i in range(capacity)] # type: ignore
+
+    def __str__(self) -> str:
+        return f"{self.capacity} {self.seats}"
+    
+    def has_free_spot():
+        return bool #True if a spot is available 
+
+    def assign_seat(name):
+        #that places someone at the table
+        return 
+
+    def left_capacity():
+        return  #that returns an integer
