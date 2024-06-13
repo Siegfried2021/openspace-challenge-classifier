7cfe77b93747dc0d088d564b438fa2a449f08a63
Siegfried2021 <lecouvetm@gmail.com>
2024-06-13 15:15:34 +0200

    openspace file

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,1 +1,32 @@
+import Table from table.py
+import random
+
+
+class Openspace:
+    def __init__(self, n):        
+        self.number_of_tables = n
+        self.tables = [Table() for _ in range(n)]
+        
+    def organize(self, names):
+        random.shuffle(names)
+        for name in names :
+            for table in self.tables:
+                while table.has_free_spot():
+                    table.assign_seat(name)
+    
+    def display():
+        
+                
+                    
+                
+                
+                     
+                 
+                        
+    
+  
+
+
+    
+
 
