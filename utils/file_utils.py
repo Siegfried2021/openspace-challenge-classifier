89d2e23108a7479c9a3e69cc927d6767d0f1c29d
carpanalper <alperc.business@gmail.com>
2024-06-14 10:24:40 +0200

    file_utils now can read from a file

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,1 +1,15 @@
 
+import pandas as pd
+from typing import List
+
+def load_names_from_excel(filepath: str) -> List[str]:
+    """
+    Load names from an Excel file.
+    Args:
+        filepath (str): The path to the Excel file.
+    Returns:
+        List[str]: A list of names.
+    """
+    df = pd.read_excel(filepath)
+    return df['Name'].tolist()
+
